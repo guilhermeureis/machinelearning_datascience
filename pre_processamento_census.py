0ac51856476cf818ff2927a905bf798077197497
Guilherme Reis <guilhermeureis@gmail.com>
2019-11-28 16:35:18 -0300

    Aula 23

--- pre_processamento_census.py
+++ pre_processamento_census.py
@@ -4,18 +4,13 @@ base = pd.read_csv('census.csv')
 previsores = base.iloc[:,0:14].values
 classe = base.iloc[:,14].values
 
-from sklearn.preprocessing import LabelEncoder
-
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
 labelencoder_previsores = LabelEncoder()
 
+onehotencorder = ColumnTransformer(transformers=[("OneHot", OneHotEncoder(), [1,3,5,6,7,8,9,13])],remainder='passthrough')
+previsores = onehotencorder.fit_transform(previsores).toarray()
 
-previsores[:,1] = labelencoder_previsores.fit_transform(previsores[:,1])
-previsores[:,3] = labelencoder_previsores.fit_transform(previsores[:,3])
-previsores[:,5] = labelencoder_previsores.fit_transform(previsores[:,5])
-previsores[:,6] = labelencoder_previsores.fit_transform(previsores[:,6])
-previsores[:,7] = labelencoder_previsores.fit_transform(previsores[:,7])
-previsores[:,8] = labelencoder_previsores.fit_transform(previsores[:,8])
-previsores[:,9] = labelencoder_previsores.fit_transform(previsores[:,9])
-previsores[:,13] = labelencoder_previsores.fit_transform(previsores[:,13])
-
+labelencorder_classe = LabelEncoder()
+classe = labelencorder_classe.fit_transform(classe)
 
