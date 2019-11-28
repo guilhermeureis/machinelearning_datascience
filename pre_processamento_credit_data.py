b86111ae6b61bfdb505bd6fddd2c22bd03f5076a
Guilherme Reis <guilhermeureis@gmail.com>
2019-11-28 14:33:59 -0300

    Aula 19

--- pre_processamento_credit_data.py
+++ pre_processamento_credit_data.py
@@ -1,7 +1,9 @@
 import pandas as pd
+import numpy as np
 
 base = pd.read_csv('credit_data.csv')
 base.describe()
+# DADOS INCONSISTENTES
 # Tratar base de dados
 # Retirada de idade negativo
 base.loc[base['age'] < 0]
@@ -16,3 +18,17 @@ base['age'].mean()
 # Substituir os valores negativos com a média
 mediaAge = base['age'][base.age > 0].mean()
 base.loc[base.age < 0, 'age'] = mediaAge
+
+
+# DADOS FALTANTES
+
+pd.isnull(base['age'])
+base.loc[pd.isnull(base['age'])]
+
+previsores = base.iloc[:, 1:4].values
+classe = base.iloc[:, 4].values
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer = imputer.fit(previsores[:, 0:3])
+previsores[:, 0:3] = imputer.transform(previsores[:,0:3])
