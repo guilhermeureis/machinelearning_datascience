05b8af76cdb03ec3a4fb7cdcb9eb52291818bec0
Guilherme Reis <guilhermeureis@gmail.com>
2019-11-28 11:05:10 -0300

    Aula 18

--- pre_processamento_credit_data.py
+++ pre_processamento_credit_data.py
@@ -2,3 +2,17 @@ import pandas as pd
 
 base = pd.read_csv('credit_data.csv')
 base.describe()
+# Tratar base de dados
+# Retirada de idade negativo
+base.loc[base['age'] < 0]
+# apagar a coluna
+base.drop('age', 1, inplace=True)
+# apagar somente os registros com problema
+base.drop(base[base.age < 0].index, inplace=True)
+# preencher os valores manualmente
+# preencher os valores com a média
+base.mean()
+base['age'].mean()
+# Substituir os valores negativos com a média
+mediaAge = base['age'][base.age > 0].mean()
+base.loc[base.age < 0, 'age'] = mediaAge
